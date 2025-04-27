20ef7eae5e92e351351cd910674f4dc99106e4fe
a_adamovich <a.adamovich@just-ai.com>
2025-04-27 14:16:00 +0300

    [code] добавил settings

---
+++ config/settings.py
@@ -0,0 +1,29 @@
+from pydantic import BaseSettings, Field
+from pathlib import Path
+
+class Settings(BaseSettings):
+    # OpenAI API
+    openai_api_key: str = Field(..., env="OPENAI_API_KEY")
+    openai_model: str = "gpt-4-turbo"
+
+    # Пути к директориям для генерации файлов
+    deploy_dir: Path = Path("deploy")
+    dags_dir: Path = deploy_dir / "dags"
+    dbt_dir: Path = deploy_dir / "dbt"
+    dbt_models_dir: Path = deploy_dir / "dbt" / "models"
+    docker_compose_path: Path = deploy_dir / "docker-compose.yml"
+
+    # DBT
+    dbt_target: str = "dev"
+
+    # ClickHouse-DWH
+    clickhouse_host: str = Field(..., env="CLICKHOUSE_HOST")
+    clickhouse_port: int = Field(..., env="CLICKHOUSE_PORT")
+    clickhouse_user: str = Field(..., env="CLICKHOUSE_USER")
+    clickhouse_password: str = Field(..., env="CLICKHOUSE_USER")
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
