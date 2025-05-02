9997608bf705f6cf50bb8a0faa563e5ba7fe7be6
a_adamovich <a.adamovich@just-ai.com>
2025-05-03 01:19:43 +0300

    [infra] дополнил конфиг для управляемости

--- config/settings.py
+++ config/settings.py
@@ -5,14 +5,23 @@ from pathlib import Path
 class Settings(BaseSettings):
     # OpenAI API
     openai_api_key: str = Field(..., env="OPENAI_API_KEY")
-    openai_model: str = "gpt-4-turbo"
+    base_url: str = "https://caila.io/api/adapters/openai"
+    llm_airflow_moving_data: str = "just-ai/gemini/gemini-2.5-pro"
+    temperature_airflow_moving_data: float = 0
+    llm_airflow_args: str = "just-ai/gemini/gemini-2.5-pro"
+    temperature_airflow_args: float = 0
+
+
 
     # Пути к директориям для генерации файлов
     deploy_dir: Path = Path("deploy")
     dags_dir: Path = deploy_dir / "dags"
     dbt_dir: Path = deploy_dir / "dbt"
-    dbt_models_dir: Path = deploy_dir / "dbt" / "models"
+    dbt_models_dir: Path = dbt_dir / "models"
     docker_compose_path: Path = deploy_dir / "docker-compose.yml"
+
+    # TEMPLATES
+    template_dag_path: Path = Path("templates") / "airflow_dag_template.py"
 
     # DBT
     dbt_target: str = "dev"
@@ -22,6 +31,13 @@ class Settings(BaseSettings):
     clickhouse_port: int = Field(..., env="CLICKHOUSE_PORT")
     clickhouse_user: str = Field(..., env="CLICKHOUSE_USER")
     clickhouse_password: str = Field(..., env="CLICKHOUSE_USER")
+
+    
+    # LLM-airflow
+    
+
+
+
 
     class Config:
         env_file = ".env"
