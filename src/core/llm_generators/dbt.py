52fbba0cfe31e1512d10400e93c26ce5c21e6001
a_adamovich <a.adamovich@just-ai.com>
2025-05-04 16:32:13 +0300

    [code] DbtGen v1

--- src/core/llm_generators/dbt.py
+++ src/core/llm_generators/dbt.py
@@ -3,6 +3,7 @@ from typing import List
 from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from src.config.settings import settings
+from src.config.prompts import prompts
 from src.core.specs import AnalyticsSpec
 
 
@@ -30,10 +31,8 @@ class DbtGenerator:
                                 api_key=settings.OPENAI_API_KEY,
                                 base_url=settings.BASE_URL
                             )
-    def _generate_dbt_project(self):
-        pass
     
-    def _generate_profiles(self):
+    def _generate_profiles(self) -> dict:
         '''
         Генерация файла profiles.yml
         '''
@@ -55,11 +54,9 @@ class DbtGenerator:
             }
         }
 
-        self._save_yml_from_dict(content=profiles,
-                                 file_path=settings.DBT_DIR / "profiles.yml")
         return profiles
 
-    def _generate_sources(self):
+    def _generate_sources(self) -> dict:
         '''
         Генерация файла models/source.yml
         '''
@@ -78,31 +75,116 @@ class DbtGenerator:
                 }
             ]
         }
-        self._save_yml_from_dict(content=sources,
-                                 file_path=settings.DBT_MODELS_DIR / "source.yml")
         return sources
     
-    def _generate_schemas(self):
+    def _generate_schemas(self) -> dict:
         pass
 
-    def _generate_stage_models(self):
-        pass
+    def _generate_stage_models(self, sources: dict) -> dict:
+        system_template = prompts.SYSTEM_PROMPT_DBT_MODELS_STAGE
+        user_template = prompts.USER_PROMPT_DBT_MODELS_STAGE
+
+        prompt_template = ChatPromptTemplate.from_messages(
+                [("system", system_template),
+                 ("user", user_template)]
+        )
+
+        chain = prompt_template | self.llm_for_models
+
+        result = chain.invoke(
+            {"sources": yaml.dump(sources, allow_unicode=True)}
+        )
+
+        return self._clean_sql_code(result.content)
 
     def _generate_intermediate_models(self) -> List[str]:
-        pass
+        system_template = prompts.SYSTEM_PROMPT_DBT_MODELS_CORE
+        user_template = prompts.USER_PROMPT_DBT_MODELS_CORE
+
+        prompt_template = ChatPromptTemplate.from_messages(
+                [("system", system_template),
+                 ("user", user_template)]
+        )
+
+        chain = prompt_template | self.llm_for_models
+
+        result = chain.invoke(
+            {}
+        )
+
+        return self._clean_sql_code(result.content)
 
     def _generate_marts(self):
-        pass
+        system_template = prompts.SYSTEM_PROMPT_DBT_MODELS_MARTS
+        user_template = prompts.USER_PROMPT_DBT_MODELS_MARTS
+
+        prompt_template = ChatPromptTemplate.from_messages(
+                [("system", system_template),
+                 ("user", user_template)]
+        )
+
+        chain = prompt_template | self.llm_for_models
+
+        result = chain.invoke(
+            {}
+        )
+
+        return self._clean_sql_code(result.content)
 
     def fill_and_save_project(self):
-        pass
-    
+        sources = self._generate_sources()
+        profiles = self._generate_profiles()
+
+        stage_models = self._generate_stage_models(sources)
+        # TO DO: вероятно, core будет зависеть от stage, а marts от core и metrics
+        core_models = self._generate_intermediate_models()
+        marts_models = self._generate_marts()
+        
+        # TO DO: схема должна на что-то опираться
+        schemas = self._generate_schemas()
+
+        self._save_yml_from_dict(content=sources,
+                                 file_path=settings.DBT_MODELS_DIR / "source.yml")
+        self._save_yml_from_dict(content=profiles,
+                                 file_path=settings.DBT_DIR / "profiles.yml")
+        self._save_yml_from_dict(content=schemas,
+                                 file_path=settings.DBT_MODELS_DIR / "schema.yml")
+        
+        for model_name, sql_code in stage_models.items():
+            self._save_sql_model(content=sql_code,
+                                 file_path=settings.DBT_MODELS_DIR / "stage" / f"stg_{model_name}.sql")
+
+        for model_name, sql_code in core_models.items():
+            self._save_sql_model(content=sql_code,
+                                 file_path=settings.DBT_MODELS_DIR / "core" / f"int_{model_name}.sql")
+        
+        for model_name, sql_code in marts_models.items():
+            self._save_sql_model(content=sql_code,
+                                 file_path=settings.DBT_MODELS_DIR / "marts" / f"{model_name}.sql")
+            
     @staticmethod
     def _save_yml_from_str(content: str, file_path: str) -> None:
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(content)
+    
     @staticmethod
     def _save_yml_from_dict(content: dict, file_path: str) -> None:
         with open(file_path, 'w', encoding='utf-8') as f:
             yaml.dump(content, f, sort_keys=False, allow_unicode=True)
+    
+    @staticmethod
+    def _save_sql_model(content: str, file_path: str) -> None:
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(content)
+    
+    @staticmethod
+    def _clean_sql_code(code_str: str) -> str:
+        # убрать обрамление ``` или ```sql и оставить только содержимое
+        pattern = r"```(?:sql)?\n(.*?)```"
+        matches = re.findall(pattern, code_str, re.DOTALL)
+        if matches:
+            # если несколько блоков, объединяем их через 2 перевода строки
+            return "\n\n".join(match.strip() for match in matches)
+        return code_str.strip()
+
 
