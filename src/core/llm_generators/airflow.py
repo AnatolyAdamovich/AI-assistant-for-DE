91669b95d879539f4a0004b72bb09c770cf35659
a_adamovich <a.adamovich@just-ai.com>
2025-05-06 02:35:55 +0300

    [code] вынес все промпты из AirflowDagGen и адаптировал под множественный источник

--- src/core/llm_generators/airflow.py
+++ src/core/llm_generators/airflow.py
@@ -15,7 +15,7 @@ from src.core.specs import AnalyticsSpec
 class AirflowDagGenerator:
     def __init__(self, analytics_specification: AnalyticsSpec):
         
-        self.data_source = analytics_specification.data_source
+        self.data_sources = analytics_specification.data_sources
         self.business_process = analytics_specification.business_process
 
         self.llm_for_moving = ChatOpenAI(
@@ -39,18 +39,9 @@ class AirflowDagGenerator:
         
     def _generate_dag_args(self) -> str:
         # Генерируем schedule_interval и start_date на основе business_process
-        system_template = (
-            "You are an experienced data engineer. Your task is to choose the correct Airflow schedule_interval and start_date for a DAG, "
-            "based on the following business process and recommendations. \n"
-            "Return only the values in Python code format"
-            "(for example: schedule_interval=\"@daily\"\nstart_date=datetime(2024, 1, 1))."
-        )
-        # user_template = (
-        #     "Business process:\n"
-        #     "- name: {name}\n"
-        #     "- schedule: {schedule}\n"
-        # )
-        user_template = prompts.USER_PROMPT_AIRFLOW_MOVING_DATA
+        system_template = prompts.SYSTEM_PROMPT_AIRFLOW_ARGS
+        user_template = prompts.USER_PROMPT_AIRFLOW_ARGS
+        
         prompt_template = ChatPromptTemplate.from_messages([
             ("system", system_template),
             ("user", user_template)
@@ -65,22 +56,9 @@ class AirflowDagGenerator:
         return self._clean_code(result.content)
 
     def _generate_moving_data_function(self) -> str:
-        system_template = (
-            "You are an experienced Python data engineer writing code for an Airflow DAG. " \
-            "You should write a function that exports the data from the source and transfers it to the analytical dwh."
-        )
-
-        user_template = (
-            "Implement a function def moving_data_from_source_to_dwh(**context) -> None for an Airflow DAG.\n"
-            "- The data source has the following properties: {data_source}\n"
-            "- For analytics database use ClickHouseHook('clickhouse_dwh')"
-            "- Use the appropriate Airflow connection for the database type (for example, PostgresHook(\"<name>_source\") for PostgreSQL or ClickHouseHook(\"<name>_source)\" for Clickhouse).\n"
-            "- Use only standard and popular open-source Python libraries (such as pandas, psycopg2). \n"
-            "- Add a docstring in Russian that describes what the function does.\n"
-            "- Import all needed libraries inside function.\n"
-            "- Do not add any comments, code or explanations outside the function code.\n"
-            "- Return only the function code."
-        )
+        system_template = prompts.SYSTEM_PROMPT_AIRFLOW_MOVING_DATA
+        user_template = prompts.USER_PROMPT_AIRFLOW_MOVING_DATA
+        
 
         prompt_template = ChatPromptTemplate.from_messages(
             [("system", system_template),
@@ -90,7 +68,7 @@ class AirflowDagGenerator:
         chain = prompt_template | self.llm_for_moving
 
         result = chain.invoke(
-            {"data_source": self.data_source}
+            {"data_sources": self.data_sources}
         )
         cleaned_code = self._clean_code(result.content)
         return self._indent_code_block(cleaned_code, indent=4)
