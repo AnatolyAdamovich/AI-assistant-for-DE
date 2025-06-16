818e52ec25e2155452d83ed5b85e7ce94c25c974
a_adamovich <a.adamovich@just-ai.com>
2025-06-16 21:22:32 +0300

    [code] обновил скрипт для CLI

--- main_cli.py
+++ main_cli.py
@@ -1,33 +1,32 @@
 from src.core.llm_generators.airflow import AirflowDagGenerator
 from src.core.llm_generators.dbt import DbtGenerator
-from src.core.llm_generators.spec import AnalyticsSpecGenerator
+from src.core.llm_generators.specification import AnalyticsSpecGenerator
+from src.core.llm_generators.dashboards import MetabaseDashboardGenerator
+from src.config.settings import settings
 
 if __name__ == "__main__":
 
-    user_description = """
-    Мы хотим построить аналитику для интернет-магазина.
-    Основная цель — анализировать продажи и поведение покупателей, чтобы повысить выручку и оптимизировать маркетинговые кампании.
-    Пользователи аналитики — менеджеры по продажам и маркетологи.
-
-    Данные хранятся в PostgreSQL. Есть две основные таблицы:
-
-    orders: содержит информацию о заказах (order_id, product_id, timestamp, customer_id, amount)
-    customers: содержит информацию о клиентах (customer_id, name, region_id, age)
-    Ключевые метрики:
-
-    * Общая сумма продаж по дням
-    * Количество уникальных покупателей по регионам
-    * Средний чек
-
-    Данные должны обновляться ежедневно, желательно ночью в промежуток между 00:00 и 05:00.
-    В дальнейшем планируется добавить витрины для анализа повторных покупок и сегментации клиентов.
-    Важно учитывать возможные ограничения по GDPR.
-    """
+    user_description = str(input("Введите описание желаемой аналитической системы:"))
     
+    print("% Структурирование информации %")
     analytics_spec = AnalyticsSpecGenerator() \
-                     .extract_info_from_users_desription(user_description)
+                     .generate_spec(user_description)
     
-    AirflowDagGenerator(analytics_spec).fill_and_save_template()
-    DbtGenerator(analytics_spec).fill_and_save_project()
-
-    print("Проект готов! Файлы сохранены в папке deploy/")
+    print("% Генерация Airflow DAG %")
+    airflow_dag = AirflowDagGenerator(analytics_spec) \
+                  .generate_dag()
+    
+    print("% Генерация DBT проекта %")
+    dbt_project = DbtGenerator(analytics_spec) \
+                  .generate_project()
+    
+    print("% Генерация Metabase Dashboard %")
+    metabase_dash = MetabaseDashboardGenerator(
+                        metabase_url=settings.METABASE_URL,
+                        username=settings.METABASE_USERNAME,
+                        password=settings.METABASE_PASSWORD) \
+                    .generate_dashboard(marts_schema=dbt_project['marts'],
+                                        metrics=analytics_spec.metrics)
+    
+    
+    print("Аналитическая система готова!")
